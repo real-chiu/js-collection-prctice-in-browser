a93b7d4f2e1c1a045dd1ba8b3ba0fd6c212043e1
real-chiu <real.chiu@oocl.com>
2020-04-02 20:51:44 +0800

    finished 3-4

--- practice/section-3/practice-4.js
+++ practice/section-3/practice-4.js
@@ -1,5 +1,49 @@
 'use strict';
 
 function createUpdatedCollection(collectionA, objectB) {
-  return 'Implement the practice require, and begin changing code in this row';
+  let object = {};
+  collectionA.forEach(key => {
+    let extraValue = 0;
+    let keyToAddExtraValue = '';
+    
+    if (key.indexOf('-') > -1) { 
+      // if element includes "-"
+      let keyCharArray = key.split("");
+      keyToAddExtraValue = keyCharArray[0];
+      extraValue = keyCharArray[2];
+      if (!object[keyToAddExtraValue]) { 
+        // if key already exists
+        object[keyToAddExtraValue] = extraValue;
+      } else {
+        object[keyToAddExtraValue] += extraValue;
+      }
+    } else {
+      if (!object[key]) { 
+        // if key already exists
+        object[key] = 1;
+      } else {
+        object[key] += 1
+      }
+    }
+  });
+
+  let objectArray = [];
+  for (let [key, value] of Object.entries(object)) {
+    objectArray.push({
+      key,
+      count: value
+    });
+  };
+
+  const { value } = objectB;
+  return objectArray.map(e => {
+    if (value.includes(e.key)) {
+      const minusValue = Math.floor(e.count / 3);
+      return {
+        key: e.key,
+        count: e.count - minusValue
+      }
+    }
+    return e;
+  });
 }
