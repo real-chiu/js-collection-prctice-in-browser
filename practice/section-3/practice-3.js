3e1f39ca2c19cbe81212b1c99e23eb4bfb52df01
real-chiu <real.chiu@oocl.com>
2020-04-02 20:51:32 +0800

    finished 3-3

--- practice/section-3/practice-3.js
+++ practice/section-3/practice-3.js
@@ -1,5 +1,35 @@
 'use strict';
 
 function createUpdatedCollection(collectionA, objectB) {
-  return 'Implement the practice require, and begin changing code in this row';
+  let object = {};
+
+  //count for appearance of char and save into object
+  collectionA.forEach(key => {
+    if (!object[key]) {
+      // if key already exists
+      object[key] = 1;
+    } else {
+      object[key] += 1
+    }
+  });
+
+  let objectArray = [];
+  for (let [key, value] of Object.entries(object)) {
+    objectArray.push({
+      key,
+      count: value
+    });
+  };
+
+  const { value } = objectB;
+  return objectArray.map(e => {
+    if (value.includes(e.key)) {
+      const minusValue = Math.floor(e.count / 3);
+      return {
+        key: e.key,
+        count: e.count - minusValue
+      }
+    }
+    return e;
+  });
 }
